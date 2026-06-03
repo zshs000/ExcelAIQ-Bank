676d32999fd003e4105c894fecfcd48b1d56bf6e
zhoujiaxin0428 <1218991098@qq.com>
2026-06-03 20:38:16 +0800

    chore: 清理重复注释、展开通配符import、统一Slf4j日志

--- eaqb-gateway/src/main/java/com/zhoushuo/eaqb/gateway/exception/GlobalExceptionHandler.java
+++ eaqb-gateway/src/main/java/com/zhoushuo/eaqb/gateway/exception/GlobalExceptionHandler.java
@@ -34,8 +34,6 @@ public class GlobalExceptionHandler implements ErrorWebExceptionHandler {
         // 响参
         Response<?> result;
         // 根据捕获的异常类型，设置不同的响应状态码和响应消息
-
-        // 根据捕获的异常类型，设置不同的响应状态码和响应消息
         if (ex instanceof NotLoginException) { // 未登录异常
             // 设置 401 状态码
             response.setStatusCode(HttpStatus.UNAUTHORIZED);
